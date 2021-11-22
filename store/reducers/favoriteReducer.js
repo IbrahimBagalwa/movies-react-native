0cb4944c37e9297ec66f3f5f113719ae94830059
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-11-23 00:56:03 +0200

    reducer favorite film

--- store/reducers/favoriteReducer.js
+++ store/reducers/favoriteReducer.js
@@ -4,14 +4,24 @@ function toggleFavorite(state = intialState, action){
     let nextState;
     switch(action.type){
         case 'TOGGLE_FAVORITE':
+            // on verifier si le film fait deja partie de nos vavories
             const favoriteFilmIndex = state.favoriteFilms.findIndex(item => item.id === action.value.id);
             if(favoriteFilmIndex !== -1){
                 // on supprime le film du favorie
+                nextState = {
+                    ...state,
+                    favoriteFilms: favoriteFilms.filter((item, index)=> index !== favoriteFilmIndex)
+                }
             }else{
                 // on ajoute le film parmi le favorie
+                nextState = {
+                    ...state,
+                    favoriteFilms: [...state.favoriteFilms, action.value]
+                }
             }
             return nextState || state
         default:
             return state
     }
 }
+export default toggleFavorite
