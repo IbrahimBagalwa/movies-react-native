b9fa4d35a73b080896730dc42dde720984a7060f
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-11-24 23:05:19 +0200

    list film

---
+++ components/FilmList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+
+
+class FilmList extends React.Component{
+    render(){
+        return(
+            <Text>Films list</Text>
+        )
+    }
+}
+
+export default FilmList;
