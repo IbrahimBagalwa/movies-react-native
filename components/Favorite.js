c3d1566bcbae94bebfe4213493cbbb8adc4ab7cf
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-11-25 11:40:31 +0200

    fix favorite film

--- components/Favorite.js
+++ components/Favorite.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, Text } from "react-native";
 import FilmList from "./FilmList";
-
+import {connect} from 'react-redux';
 class Favorite extends React.Component{
     render(){
         return(
@@ -18,4 +18,10 @@ const styles = StyleSheet.create({
     
 })
 
-export default Favorite;
+const mapStateToProps = state =>{
+   return { 
+    favoriteFilms: state.favoriteFilms 
+   }
+}
+
+export default connect(mapStateToProps)(Favorite);
