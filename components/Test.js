53dfeffa1db33348cf1f3cfc86cf734ba1523fca
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-11-26 13:01:38 +0200

    test specific development

---
+++ components/Test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+class Test extends React.Component{
+    render(){
+        return(
+            <Text>Hello Test</Text>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default Test
